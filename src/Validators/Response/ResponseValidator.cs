92d50fecb435e113186625bdbb229ee72779e513
YuriLopess <costalopesyuri@gmail.com>
2025-04-13 16:05:41 -0300

    feat: add methods in validators package

--- src/Validators/Response/ResponseValidator.cs
+++ src/Validators/Response/ResponseValidator.cs
@@ -1,4 +1,5 @@
-﻿using src.Models;
+﻿using src.Exceptions;
+using src.Models;
 
 namespace src.Validators.Response
 {
@@ -6,7 +7,17 @@ namespace src.Validators.Response
     {
         public void ValidateResponse<T>(ResponseModel<T> response)
         {
-            throw new NotImplementedException();
+            if (response is null)
+                throw new DomainValidationException("Response object cannot be null.");
+
+            if (response.Status && response.Data == null)
+                throw new DomainValidationException("Successful responses must contain data.");
+
+            if (!response.Status && string.IsNullOrWhiteSpace(response.Message))
+                throw new DomainValidationException("Error responses must contain a message.");
+
+            if (response.Message.Length > 300)
+                throw new DomainValidationException("Message must be at most 300 characters long.");
         }
     }
 }
