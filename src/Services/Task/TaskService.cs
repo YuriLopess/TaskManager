1be2d0aa8b6df4e8cf680dc59a8c49724b529ed5
YuriLopess <costalopesyuri@gmail.com>
2025-04-27 14:33:12 -0300

    feat: add new methods in service package

--- src/Services/Task/TaskService.cs
+++ src/Services/Task/TaskService.cs
@@ -1,6 +1,28 @@
-﻿namespace src.Services.Task
+﻿using src.Dto;
+using src.Models;
+
+namespace src.Services.Task
 {
     public class TaskService : ITaskService
     {
+        public Task<ResponseModel<List<TaskModel>>> DeleteTask(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<TaskModel>>> GetTask(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<TaskModel>>> PostTask(TaskDTO task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<TaskModel>>> PutTask(TaskDTO task)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/Services/User/UserService.cs
+++ src/Services/User/UserService.cs
@@ -1,13 +1,48 @@
-﻿using src.Dto;
+﻿using System.Net;
+using Microsoft.EntityFrameworkCore;
+using src.Data;
+using src.Dto;
 using src.Models;
 
 namespace src.Services.User
 {
     public class UserService : IUserService
     {
-        public Task<ResponseModel<List<UserModel>>> DeleteUser(Guid id)
+        private readonly AppDbContext _context;
+
+        public UserService(AppDbContext context) {
+            _context = context;
+        }
+
+        public async Task<ResponseModel<List<UserModel>>> DeleteUser(Guid id)
         {
-            throw new NotImplementedException();
+            ResponseModel<List<UserModel>> response = new ResponseModel<List<UserModel>();
+
+            try 
+            {
+                var user = _context.Users.FirstOrDefault(userDb => userDb.Id == id);
+
+                if (user == null)
+                {
+                    response.Message = "No records found";
+                    return response;
+                }
+
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+
+                response.Data = await _context.Users.ToListAsync();
+                response.Message = "Record deleted successfully";
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Status = false;
+                return response;
+            }
+
         }
 
         public Task<ResponseModel<List<UserModel>>> GetUser(Guid id)
